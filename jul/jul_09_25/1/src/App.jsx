db170a9471b4a9ff793074314a1a484f11d8ef57
Soumik Hazra <www.soumikhazra@gmail.com>
2025-07-09 19:41:32 +0530

    solana web adapter

---
+++ jul/jul_09_25/1/src/App.jsx
@@ -0,0 +1,37 @@
+import { useMemo } from 'react';
+import { ConnectionProvider,WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { WalletModalProvider,WalletDisconnectButton,WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+import '@solana/wallet-adapter-react-ui/styles.css';
+
+
+import { SendTokens } from './SendTokens';
+import { SignMessage } from './SignMessage'; 
+import { RequestAirdrop } from './RequestAirdrop';
+import { ShowSolBalance } from './ShowSolBalance'
+
+
+function App() {
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  return (
+      <ConnectionProvider endpoint={endpoint}>
+          <WalletProvider wallets={[]} autoConnect>
+              <WalletModalProvider>
+                <div style={{ display: 'flex', justifyContent: "space-between" }}>
+                  <WalletMultiButton />
+                  <WalletDisconnectButton />
+                </div>
+                 <RequestAirdrop />
+                <ShowSolBalance /> 
+                {/* <Tokens /> */}
+                <SignMessage />
+                <SendTokens />
+              </WalletModalProvider>
+          </WalletProvider>
+      </ConnectionProvider>
+  );
+}
+
+export default App
